737108bdf68b666096918caa1f35475d53188eb1
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2022-01-05 11:35:17 -0300

    update

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// get HTML elements
+const adults = document.getElementById("nAdults");
+const kids = document.getElementById("nKids");
+const hours = document.getElementById("nHours");
+const calcButton = document.getElementById("calcButton");
+
+// variables
+
+// add events to the elements
+calcButton.addEventListener("click", calculate);
+
+// functions
+function calculate() {
+ 
+}
